a2d704b809975021e216cdde7032cb0ab3daabaa
Aman Kumar <107931590+amankr9931@users.noreply.github.com>
2022-08-19 13:18:56 +0530

    Add files via upload

---
+++ New folder (2)/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "which is the national animal of India?",
+    answer: "Tiger",
+    options: [
+      "Lion",
+      "Tiger",
+      "Elephant",
+      "Deer"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which day comes after Friday?",
+    answer: "Saturday",
+    options: [
+      "Monday",
+      "Wednesday",
+      "Sunday",
+      "Saturday"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is our national flower?",
+    answer: "Lotus",
+    options: [
+      "Lotus",
+      "Rose",
+      "Sun flower",
+      "Lilly"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Where does the sun set?",
+    answer: "West",
+    options: [
+      "North",
+      "South",
+      "East",
+      "West"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How many months do we have in a year?",
+    answer: "12",
+    options: [
+      "12",
+      "11",
+      "10",
+      "9"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
